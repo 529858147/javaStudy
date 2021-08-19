599a36555f8c50e84777ffd1b8a5f42d6c850e0d
admin <louis_lai@qq.com>
2021-08-19 22:08:20 +0800

    java-BlockingQueue

--- src/main/java/com/how2j/thread/concurrent/ThreadPoolExecutorDemo.java
+++ src/main/java/com/how2j/thread/concurrent/ThreadPoolExecutorDemo.java
@@ -1,9 +1,6 @@
 package com.how2j.thread.concurrent;
 
-import java.util.concurrent.LinkedBlockingQueue;
-import java.util.concurrent.ThreadFactory;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
@@ -19,13 +16,13 @@ public class ThreadPoolExecutorDemo {
         Service service = new Service();
         RunThread runThread = new RunThread(service);
         //超过线程池大小时，会报拒绝执行错误
-        LinkedBlockingQueue linkedBlockingQueue = new LinkedBlockingQueue(5);
+        BlockingQueue<Runnable> blockingQueue = BlockingQueueEnum.ARRAY_BLOCKING_QUEUE.getBlockingQueue();
         ThreadPoolExecutor executor = new ThreadPoolExecutor(
                 3,//核心池的大小（即线程池中的线程数目大于这个参数时，提交的任务会被放进任务缓存队列）
                 5,//线程池最大能创建的线程数
                 10,//除核心池外的线程，超过核心池数量的线程能存活的时间
                 TimeUnit.SECONDS,//存活时间的单位
-                linkedBlockingQueue,//存放任务的阻塞队列
+                blockingQueue,//存放任务的阻塞队列
                 new ThreadFactoryTest(),//创建线程的线程工厂
                 new ThreadPoolExecutor.CallerRunsPolicy()//当任务数量达到阻塞队列和线程池最大线程数量时，拒绝执行任务的策略
         );
@@ -86,4 +83,22 @@ class ThreadFactoryTest implements ThreadFactory {
     }
 }
 
+enum BlockingQueueEnum {
+    ARRAY_BLOCKING_QUEUE(new ArrayBlockingQueue(5)),
+    LINKED_BLOCKING_QUEUE(new LinkedBlockingQueue(5)),
+    PRIORITY_BLOCKING_QUEUE(new PriorityBlockingQueue()),
+    DELAY_QUEUE(new DelayQueue()),
+    SYNCHRONOUS_QUEUE(new SynchronousQueue()),
+    LINKED_TRANSFER_QUEUE(new LinkedTransferQueue()),
+    LINKED_BLOCKING_DEQUE(new LinkedBlockingDeque());
 
+    private BlockingQueue<Runnable> blockingQueue;
+
+    BlockingQueueEnum(BlockingQueue blockingQueue) {
+        this.blockingQueue = blockingQueue;
+    }
+
+    public BlockingQueue<Runnable> getBlockingQueue() {
+        return blockingQueue;
+    }
+}
