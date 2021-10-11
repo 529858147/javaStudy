98836acacfde6c4fbbbb6a70f1b75dc8ac4123d3
admin <louis_lai@qq.com>
2021-10-11 22:24:16 +0800

    java-ArrayList

---
+++ src/main/java/com/how2j/container/collection/list/ArrayListDemo.java
@@ -0,0 +1,16 @@
+package com.how2j.container.collection.list;
+
+import java.util.ArrayList;
+
+/**
+ * @author louis
+ * @Title:
+ * @Package
+ * @Description: 学习ArrayList类的使用
+ * @date 2021/10/11 21:23
+ */
+public class ArrayListDemo {
+    public static void main(String[] args) {
+        ArrayList<Object> arrayList = new ArrayList<>();
+    }
+}
